dabd1bafad45565ab18c878289be38aa3352d4eb
Joanna <askat58@gmail.com>
2021-03-20 10:50:28 +0000

    Add local repository to GitHub

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from 'axios';
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${Math.round(response.data.main.temp)}℃`);
+
+  }
+  let apiKey = "856531c30ebeae24c48d2152bc04716e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+
+    <Loader
+        type="ThreeDots"
+        color="violet"
+        height={200}
+        width={200} 
+      />
+  )
+}
+  
